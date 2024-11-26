72a2aecf2eec732942f7b4ad2a3843e122877a09
Sean Maruel Anub Comingues <maui@Seans-Mac-mini.local>
2024-11-26 10:46:55 +0800

    fixes

--- src/app/(mobile)/customer/orders/[id]/_components/pendings-table.tsx
+++ src/app/(mobile)/customer/orders/[id]/_components/pendings-table.tsx
@@ -30,7 +30,7 @@ export default function PendingTables({ transactions, setSelectedTransaction}: {
                         <TableRow onClick={() => setSelectedTransaction(transaction)} key={transaction.id} className=" py-5">
                             <TableCell className="font-medium">{format(transaction.createdAt, "P hh:mm aa")}</TableCell>
                             <TableCell className=" text-xs"><BadgeStatus status={transaction.status} /></TableCell>
-                            <TableCell className="text-right">{formatCurrency(transaction.total_amount + (transaction.delivery_fee||0))}</TableCell>
+                            <TableCell className="text-right">{formatCurrency(transaction.total_amount)}</TableCell>
                         </TableRow>
                     ))}
                 </TableBody>
